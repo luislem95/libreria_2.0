8f32af02daaf7acead85efa461074cf0557f805a
Luis Lemus <rodrigo_mendoza95@hotmail.com>
2023-05-05 10:38:48 -0600

    library just to handle stuff and sales and sends

--- Componentes/index.php
+++ Componentes/index.php
@@ -1,7 +1,7 @@
 <?php include '../parts/header.php'; ?>
 
 <section id="sect1" >
-    <video src="../img/indexVideo.mp4" autoplay="true" muted="true" loop="true" poster="https://carontestudio.com/img/contacto.jpg"  style="width:100%;"></video>
+    <video src="../img/index.mp4" autoplay="true" muted="true" loop="true" poster="https://carontestudio.com/img/contacto.jpg"  style="width:100%;"></video>
   </section>
 
 <?php include '../parts/footer.php'; ?>
